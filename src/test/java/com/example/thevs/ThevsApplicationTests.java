4cb2f08bea347e78db5eb3e1cc65af112411f125
vishal shete <shetevishal1999@gmail.com>
2023-12-24 20:48:22 +0530

    is it first

---
+++ src/test/java/com/example/thevs/ThevsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thevs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThevsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
